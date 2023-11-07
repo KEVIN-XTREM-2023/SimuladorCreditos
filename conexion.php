e2e8c7f4b2a3a7f5fabf07846dd0cf3e19eed218
Kevin-Xtrem <kevinxtrem2022@gmail.com>
2023-11-07 14:46:25 -0500

    Cambio Conexion

--- conexion.php
+++ conexion.php
@@ -6,12 +6,12 @@ $password = "";
 $dbname = "simuladorcreditos";
 
 $conexion = mysqli_connect($servername,$user,$password,$dbname);
-//$conexion = new mysqli($servername,$user,$password,$dbname);
+$mysqli = new mysqli($servername,$user,$password,$dbname);
 
 if (!$conexion) {
     die("Error en la conexion".mysqli_connect_error());
 } else {
-    echo "Conexion exitosa";
+    //echo "Conexion exitosa";
 }
 
 ?>
